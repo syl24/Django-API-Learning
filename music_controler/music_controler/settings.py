b55067a77098e9df2b53b57c8f2ee5d89fd0ea87
syl24 <syl24@live.com>
2021-05-09 22:20:44 -0700

    commit

--- music_controler/music_controler/settings.py
+++ music_controler/music_controler/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jkp(10#ew9&!m1a0ddde964!l1cq#c=#9p#7s#+h++j!uuq^cz'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
