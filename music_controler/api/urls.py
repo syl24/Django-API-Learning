05d2d8236e0e80000ddbe175bf31e393a2f4f1d3
syl24 <syl24@live.com>
2021-05-18 23:13:30 -0700

    get request for room, and room logic made

--- music_controler/api/urls.py
+++ music_controler/api/urls.py
@@ -1,9 +1,10 @@
 # from music_controler.api.models import Room
 from django.contrib import admin
 from django.urls import path
-from .views import CreateRoomView, RoomView
+from .views import CreateRoomView, RoomView, GetRoom
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view()),
 ]
--- music_controler/frontend/src/components/HomePage.js
+++ music_controler/frontend/src/components/HomePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import { BrowserRouter as Router , Switch, Link, Redirect, Route } from "react-router-dom";
-
+import Room from "./Room";
 
 export default class HomePage extends Component{
     constructor(props){
@@ -18,6 +18,7 @@ export default class HomePage extends Component{
                     <Route exact path="/"> <p>This is the Home Page</p> </Route>
                     <Route path="/join" component={RoomJoinPage}/>
                     <Route path="/create" component={CreateRoomPage}/>
+                    <Route path="/room/:roomCode" component={Room} />
 
                 </Switch>
 
